78a4803d96166ba8816e3cdde317207a3db6194b
Jordan <jordancharest3@gmail.com>
2018-02-21 22:56:03 -0500

    Refactoring, finished SRT

--- process.h
+++ process.h
@@ -29,6 +29,7 @@ typedef struct simulation_statistics {
 class Process {
 public:
     // Constructor
+    Process() : pid('?'), arrival_time(0), burst_time(0), num_bursts(0), total_bursts(0), io_time(0) {}
     Process(char _pid, int _a_time, int _b_time, int _num_b, int _io) :
         pid(_pid), arrival_time(_a_time), burst_time(_b_time), num_bursts(_num_b), total_bursts(_num_b), io_time(_io) {}
 
@@ -41,11 +42,14 @@ public:
     const int getIOTime() const { return io_time; }
     const Status getStatus() const { return s; }
     const int getStartTime() const { return start_time; }
+    const int endRemainingTime() const { return (start_time + remaining_time); }
     const int endBurstTime() const { return (start_time + burst_time); }
     const int endIOTime() const { return (start_time + io_time); }
+    bool wasPreempted() const { return preempted; }
 
     // Modifiers
     void decrementNumBursts() { num_bursts--; }
+    void noLongerPreempted() { preempted = false; }
     void setAsTERMINATED() { s = Status::TERMINATED; }
     void setAsREADY(int time) {
         ready_time = time;
@@ -59,6 +63,14 @@ public:
         start_time = time + T_CS/2;
         s = Status::BLOCKED;
     }
+    void preempt(int time) {
+        if (this->preempted)
+            remaining_time = start_time + remaining_time - time;
+        else {
+            remaining_time = start_time + burst_time - time;
+            preempted = true;
+        }
+    }
 
 private:
     char pid;
@@ -69,6 +81,8 @@ private:
     int io_time;
     int start_time = 0; // used for both CPU bursts and IO blocks
     int ready_time = 0; // the time the process was last added to the ready queue
+    int remaining_time = 0; // time remaining in CPU burst after a preemption
+    bool preempted = false;
     Status s = Status::READY;
 };
 
@@ -81,11 +95,12 @@ stat_t Round_Robin(std::vector<Process> &processes);
 
 std::string queue_contents(std::list<Process> &process_queue);
 void process_arrival(std::list<Process> &ready_queue, Process &proc, int time);
+void preempt_on_arrival(std::list<Process> &ready_queue, Process &arriving, Process &running, int time);
 void process_start(std::list<Process> &ready_queue, Process &proc, int time);
+void process_finished_burst(std::list<Process> &ready_queue, std::list<Process> &IO_blocked, Process &proc, int* CPU_available, stat_t* stats, int time);
 void process_block(std::list<Process> &ready_queue, std::list<Process> &IO_blocked, Process &proc, int time);
 void process_finished_IO(std::list<Process> &ready_queue, std::list<Process> &IO_blocked, int time);
 void process_termination(std::list<Process> &ready_queue, Process &proc, int time);
-
 
 
 
--- shortest_remaining_time.cpp
+++ shortest_remaining_time.cpp
@@ -3,6 +3,11 @@
 #include <list>
 #include "process.h"
 
+// SHORTEST REMAINING TIME SORT ==================================================================
+bool SRT_sort(Process &a, Process &b) {
+    return (a.getBurstTime() < b.getBurstTime());
+}
+
 
 // SHORTEST REMAINING TIME =======================================================================
 /* Control function for shortest remaining time simulator                                       */
@@ -15,9 +20,110 @@ stat_t Shortest_Remaining_Time(std::vector<Process> &processes) {
 
     // Build the initial ready queue, processes are ordered by arrival time
     int time = processes[0].getArrivalTime();
+    int next = 0;
 
+    // check for all processes that will arrive before the first process can start running
+    for (int i = 0; i < (T_CS/2); i++) {
+        while (processes[next].getArrivalTime() == time + i ) {
+            process_arrival(ready_queue, processes[next], time+i);
+            next++;
+        }
+    }
+
+    // start the first process
+    Process running = ready_queue.front();
+    process_start(ready_queue, running, time+T_CS/2);   // allow time for the half context switch on the first process
+
+
+
+    bool preemption = false;
+    Process preempting_process;
+    int CPU_available = 0;
+    int context_counter = 0;
+    int num_bursts = 1;
+    int total_burst_time = 0;
+    std::list<Process> IO_blocked;
+
+    while (next < total_processes  ||  ready_queue.size() > 0  ||  IO_blocked.size() > 0  ||  running.getStatus() == Status::RUNNING) {
+
+        // preempt the currently running process
+        if (preemption) {
+            if (context_counter == 0) {
+                running.preempt(time-1);    // preemption actually occurred last ms
+                ready_queue.push_back(running);
+                ready_queue.sort(SRT_sort);
+
+                running = preempting_process;
+            }
+            context_counter++;
+
+            // context switch is completed
+            if (context_counter >= T_CS) {
+                process_start(ready_queue, running, time);
+                context_counter = 0;
+                preemption = false;
+            }
+
+        // check if a new process should be started (don't check for this if a preemption is occurring)
+        } else if (running.getStatus() != Status::RUNNING  &&  ready_queue.size() > 0) {
+            context_counter++;
+
+            if (time >= CPU_available  &&  context_counter >= T_CS/2) {
+                running = ready_queue.front();
+                process_start(ready_queue, running, time);
+
+                num_bursts++;
+                context_counter = 0;
+            }
+
+        // check if the current running process is done using the CPU (don't check for this if a preemption is occurring)
+        // or
+        // check if a previously preempted process is done using the CPU
+        } else if ((running.getStatus() == Status::RUNNING  &&  (running.endBurstTime() == time))
+                    || (running.wasPreempted()  &&  running.getStatus() == Status::RUNNING  &&  running.endRemainingTime() == time)){
+            total_burst_time += (time - running.getStartTime());
+            process_finished_burst(ready_queue, IO_blocked, running, &CPU_available, &stats, time);
+        }
+
+        // check if any processes are arriving
+        if (processes[next].getArrivalTime() == time) {
+            if ((processes[next].getBurstTime() < (running.endBurstTime() - time))
+                || (processes[next].getBurstTime() < (running.endRemainingTime() - time)) ) {
+
+                preemption = true;
+                preempting_process = processes[next];
+                preempt_on_arrival(ready_queue, preempting_process, running, time);
+
+            } else {
+                process_arrival(ready_queue, processes[next], time);
+                ready_queue.sort(SRT_sort);
+            }
+
+            next++;
+        }
+
+        // check if any process is done IO
+        if (IO_blocked.front().endIOTime() == time) {
+
+            if ((IO_blocked.front().getBurstTime() < (running.endBurstTime() - time))
+                || (processes[next].getBurstTime() < (running.endRemainingTime() - time)) )  {
+
+                preemption = true;
+                preempting_process = IO_blocked.front();
+                std::cout << "time " << time << "ms: Process " << preempting_process.getPID()
+                          << " completed I/O and will preempt " << running.getPID() << " " << queue_contents(ready_queue) << "\n";
+                ready_queue.push_front(preempting_process);
+                IO_blocked.pop_front();
+            } else {
+                process_finished_IO(ready_queue, IO_blocked, time);
+                ready_queue.sort(SRT_sort);
+            }
+        }
+
+        time++;
+    }
+
+    time += (T_CS/2 - 1);   // allow time for context switch
     std::cout << "time " << time << "ms: Simulator ended for SRT\n" << std::endl;
     return stats;
-
-
 }
